ffb00409a1fdaad87216e27d14ea9d9d1ea8d593
Robin-singh24 <robin.4github@gmail.com>
2025-11-09 02:58:16 +0530

    Some more JWT AUTH

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import express from 'express';
-import { authAdmin } from './middlewares/auth.js';
-import connectDB from './config/database.js';
 import bycrypt from 'bcrypt';
 import cookieParser from 'cookie-parser';
 import jwt from 'jsonwebtoken';
@@ -10,10 +8,11 @@ import jwt from 'jsonwebtoken';
 import { validateUserSignUpData } from './utils/validation.js'
 import hashedPassword from './utils/hashPassword.js';
 import User from './models/user.js';
+import connectDB from './config/database.js';
+import { userAuth } from './middlewares/auth.js';
+
 
 const app = express();
-
-const PORT = 3000;
 
 //MIDDLEWARES
 app.use(express.json());
@@ -57,9 +56,10 @@ app.post('/login', async (req, res) => {
         const isValidPassword = await bycrypt.compare(password, user.password);
         if (isValidPassword) {
             //jwt token to be implemented
-            const token = await jwt.sign({ _id: user._id }, "Dev@TinderSecret");
-            
-            res.cookie("token", token);
+            const currentTimeInSeconds = Math.floor(Date.now() / 1000);
+            const token = jwt.sign({ _id: user._id }, "Dev@TinderSecret", { expiresIn: currentTimeInSeconds + (60 * 10) });//token valid for 10 minutes
+
+            res.cookie("token", token,{ expires: new Date(Date.now() + 24 * 3600000)}); // cookie valid for 24 hours
             res.send("Login Successful...");
         } else {
             throw new Error("Invalid Credentials!!!");
@@ -70,119 +70,26 @@ app.post('/login', async (req, res) => {
     }
 })
 
-//GET user by ID
-app.get('/user/:userId', async (req, res) => {
-    const userId = req.params.userId;
-    try {
-        const user = await User.findById(userId);
-        if (!user) {
-            res.status(404).send("No user found");
-        } else {
-            res.send(user);
-        }
-    } catch (error) {
-        res.status(400).send("something went wrong", err);
-    }
-})
-
-//Particular User API - GET /user - get a particular user from the database using email
-app.get('/user', async (req, res) => {
-    const userEmail = req.body.email;
-
-    try {
-        const users = await User.findOne({ email: userEmail })
-        if (!users) {
-            res.status(404).send("No user found");
-        } else {
-            res.send(users);
-        }
-    } catch (err) {
-        res.status(400).send("something went wrong", err);
-    }
-})
-
-//Feed API - GET /feed - get all users from the database 
-app.get('/feed', async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users)
-    } catch (error) {
-        res.send("Error fetching users", error);
-    }
-})
-
-//delete user API - DELETE /user
-app.delete('/user', async (req, res) => {
-    const userId = req.body.userId;
-
-    try {
-        const user = await User.findByIdAndDelete(userId);
-        if (!user) {
-            res.status(404).send("No user found");
-        } else {
-            res.send("User deleted successfully");
-        }
-    } catch (error) {
-        res.status(400).send("Error deleting user", error);
-    }
-})
-
-//UPDATE user API - PATCH /user
-app.patch('/user/:userId', async (req, res) => {
-    const userId = req.params?.userId;
-    const data = req.body;
-
-    try {
-
-        const ALLOWED_UPDATES = ['password', 'lastName', 'age', 'about', 'gender', 'photoUrl', 'skills'];
-        const hashedPass = await hashedPassword(req.body.password);
-        req.body.password = hashedPass;
-        const isUpdateAllowed = Object.keys(data).every((k) =>
-            ALLOWED_UPDATES.includes(k)
-        );
-        if (!isUpdateAllowed) {
-            throw new Error("Update not allowed");
-        }
-        if (data.skills && data.skills.length > 10) {
-            throw new Error("Skills cannot be more than 10");
-        }
-        const updatedUser = await User.findByIdAndUpdate(userId, data, {
-            returnDocument: true,
-            runValidators: true,
-        });
-        if (!updatedUser) {
-            res.status(404).send("No user found");
-        } else {
-            res.send("User updated successfully");
-        }
-    } catch (error) {
-        res.status(400).send("Error updating user: " + error.message);
-    }
-})
-
 //GET own profile - GET /profile
-app.get('/profile', async (req, res) => {
+app.get('/profile', userAuth, async (req, res) => {
     try {
-        const cookies = req.cookies;
+        const user = req.user;
 
-        const { token } = cookies;
-        if(!token){
-            throw new Error("Invalid Token");
-        }
-        //Validate the token
-        const decodedMessage = await jwt.verify(token, "Dev@TinderSecret");
-
-        const { _id } = decodedMessage;
-        const user = await User.findById(_id);
-        if(!user){
-            throw new Error("No user found");
-        }
         res.send(user);
     } catch (error) {
         res.status(400).send("Error fetching profile: " + error.message);
     }
 })
 
+//POST /sendConnectionRequest - Send Connection Request API
+app.post('/sendConnectionRequest', userAuth, async (req, res) => {
+    const user = req.user;
+    console.log("Sending connection request...");
+    res.send(user.firstName + " is sending the Request");
+})
+
+
+const PORT = 3000;
 connectDB().then(() => {
     console.log("Database connected successfully");
     app.listen(PORT, () => {
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,9 +1,30 @@
-export const authAdmin = (req,res,next) => {
-    const token = "xyzadmin";
-    const isAuthorized = token === "xyzadmin";
-    if(!isAuthorized){
-        res.status(401).send("Unauthorized user!!!");
-    }else{
+import jwt from "jsonwebtoken";
+import User from '../models/user.js';
+
+
+const userAuth = async (req, res, next) => {
+    //Read the token from cookies
+    try {
+        const { token } = req.cookies;
+        if (!token) {
+            throw new Error("No token found");
+        }
+
+        const decodedMessage = jwt.verify(token, "Dev@TinderSecret");
+        // const { _id } = decodedMessage;
+        const user = await User.findById(decodedMessage._id);
+        if (!user) {
+            throw new Error("No user found");
+        }
+
+        req.user = user;
         next();
+    } catch (error) {
+        res.status(401).send("ERROR : " + error.message);
     }
+    //validate the token
+
+    //Find the user
 }
+
+export { userAuth };
