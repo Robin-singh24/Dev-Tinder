aa6d9e29078eea95eaf7ad292f6ef9b1a14081cb
Robin-singh24 <robin.4github@gmail.com>
2025-11-26 20:32:07 +0530

    the last API(probably)

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import { userAuth } from '../middlewares/auth.js';
 import ConnectionRequest from '../models/connectionRequest.js';
+import User from '../models/user.js';
 
 const userRouter = express.Router();
 
@@ -50,5 +51,39 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
     }
 });
 
+userRouter.get('/user/feed', userAuth, async(req,res)=>{
+    try {
 
-export default userRouter;
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 5;
+        limit = Math.min(limit,10); // max limit 20
+        const skipUsers = (page-1)*limit;
+
+        const connectionRequest = await ConnectionRequest.find({
+            $or :[
+                {toUserId : loggedInUser._id},
+                {fromUserId : loggedInUser._id}
+            ]
+        }).select("toUserId fromUserId");
+
+        const hiddenUsersFromFeed = new Set();
+
+        connectionRequest.forEach(req=>{
+            hiddenUsersFromFeed.add(req.toUserId.toString());
+            hiddenUsersFromFeed.add(req.fromUserId.toString());
+        });
+
+        const users = await User.find({
+           $and : [ {_id : {$nin : Array.from(hiddenUsersFromFeed)}}, {_id : {$ne:loggedInUser._id}}]
+        }).select(USER_SAFE_DATA).skip(skipUsers).limit(limit);
+
+        res.json({data: users});
+
+    } catch (error) {
+        res.status(400).send({message: error.message});
+    }
+});
+
+export default userRouter; 
