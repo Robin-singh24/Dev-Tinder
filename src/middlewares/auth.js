6dbe7c3a79e2e84ef4f77e659b2fb72e0784aea3
Robin-singh24 <robin.4github@gmail.com>
2025-12-02 21:24:08 +0530

    small change in middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -7,7 +7,7 @@ const userAuth = async (req, res, next) => {
     try {
         const { token } = req.cookies;
         if (!token) {
-            throw new Error("No token found");
+            return res.status(401).sned("Please Login!!!");
         }
 
         const decodedMessage = jwt.verify(token, "Dev@TinderSecret");
