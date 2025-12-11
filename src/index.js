4b69d049cb90789baf2959730b9452a9b098ff87
Robin-singh24 <robin.4github@gmail.com>
2025-12-12 03:54:05 +0530

    feat: webSockets setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ dotenv.config();
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import http from 'http'
 
 import './utils/cronJob.js';
 
@@ -16,13 +17,14 @@ import profileRouter from './routes/profile.js';
 import requestRouter from './routes/request.js';
 import userRouter from './routes/user.js';
 import paymentRouter from './routes/payment.js'
+import initialiseSocket from "./utils/socket,js";
 
 const app = express();
 
 // ====== CORS MUST BE APPLIED HERE ======
 app.use(
   cors({
-    origin:[
+    origin: [
       "http://localhost:5173",
       process.env.CLIENT_URL
     ],   // your Vercel URL
@@ -42,13 +44,17 @@ app.use('/', authRouter);
 app.use('/', profileRouter);
 app.use('/', requestRouter);
 app.use('/', userRouter);
-app.use('/',paymentRouter)
+app.use('/', paymentRouter)
+
+const server = http.createServer(app);
+
+initialiseSocket(server)
 
 // ====== START SERVER ======
 connectDB()
   .then(() => {
     console.log("Database connected successfully");
-    app.listen(process.env.PORT || 3000, () => {
+    server.listen(process.env.PORT || 3000, () => {
       console.log(`Server is running on Port ${process.env.PORT}`);
     });
   })
